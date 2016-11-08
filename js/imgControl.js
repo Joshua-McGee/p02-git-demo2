1e12c9e48bfbe996475929ca1c28d251a68845c8
RileySchina <riley.schina@gmail.com>
2016-11-07 16:40:04 -0800

    made changes to imgControl to correct errors
    added zepto to citations
    
    added commit-paragraph-black for the imgControl to use

--- js/imgControl.js
+++ js/imgControl.js
@@ -10,6 +10,52 @@ console.log("Javascript is running");
 
 var width = $(window).width();
 
+window.addEventListener("resize", function(){
+	console.log("running");
+	width = $(window).width();
+	if(width < 1056){
+
+		console.log("fired");
+
+		$("#backIMG").removeClass("commit-banner");
+		$("#belowIMG").removeClass("hidden");
+
+		
+
+	//accesability
+	// $("#nav-toggle").attr("aria-hidden", "false");
+	// $("#nav-items").attr("aria-hidden", "true");
+}
+if(width > 1056){
+	$("#backIMG").addClass("commit-banner");
+	$("#belowIMG").addClass("hidden");
+}
+
+if(width < 1161) {
+	$("#para1").removeClass("commit-paragraph");
+	$("#para1").addClass("commit-paragraph-black");
+
+	$("#para2").removeClass("commit-paragraph");
+	$("#para2").addClass("commit-paragraph-black");
+
+	$("#para3").removeClass("commit-paragraph");
+	$("#para3").addClass("commit-paragraph-black");
+}
+
+if(width > 1163) {
+	$("#para1").addClass("commit-paragraph");
+	$("#para1").removeClass("commit-paragraph-black");
+
+	$("#para2").addClass("commit-paragraph");
+	$("#para2").removeClass("commit-paragraph-black");
+
+	$("#para3").addClass("commit-paragraph");
+	$("#para3").removeClass("commit-paragraph-black");
+}
+
+});
+
+
 if(width < 1040){
 
 	$("#backIMG").removeClass("commit-banner");
